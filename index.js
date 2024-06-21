0bd06461f2540fd9acf80261a581b4ac174d2294
danHamash <megadestroier@hotmail.com>
2024-06-21 20:05:15 -0300

    codigo corrigido e formatado

--- index.js
+++ index.js
@@ -3,40 +3,40 @@ Que foi para não ter medo de explorar coisas novas. Utilizei o conhecimento adq
 Fiz o exercicio solicitado usando funções para realizar um calculadorr de partida ranqueada. junto a esse metodo solicitado pelo desafio. 
 Optei por ciar uma função randomica de geração de numeros. Para simular um jogador ganhando seus propior pontos, e integrando essa função com o restante do codigo. 
 Só solicitar o  console que imprima o resultado, sem precisar utilizar parametros manuais e manipulasdos.
-*/ 
+*/
 
 
-let resultado = calcular (numeroAleatorio(1, 120), 10 )
-let  rank = classificacao ()
+let resultado = calcular(numeroAleatorio(1, 120), 10)
+let rank = classificacao()
 
 function main() {
-    classificacao ()
-    calcular ()
-    numeroAleatorio ()
+    classificacao()
+    calcular()
+    numeroAleatorio()
 }
-function numeroAleatorio (a, b){
+function numeroAleatorio(a, b) {
     return Math.floor(Math.random() * (b - a + 1)) + a
 }
-function calcular (win, losse){
+function calcular(win, losse) {
     return win - losse
 }
-function classificacao(){
-    if (resultado < 0 ){
+function classificacao() {
+    if (resultado < 0) {
         return "Plastico"
-    }else if (resultado >= 0 && resultado <= 10){
+    } else if (resultado >= 0 && resultado <= 10) {
         return "Ferro"
-    } else if (resultado >= 11 && resultado <= 20){
+    } else if (resultado >= 11 && resultado <= 20) {
         return "Bronze"
-    }else if (resultado >= 21 && resultado <= 50){
+    } else if (resultado >= 21 && resultado <= 50) {
         return "Prata"
-    }else if (resultado >= 51 && resultado <= 80){
+    } else if (resultado >= 51 && resultado <= 80) {
         return "Ouro"
-    }else if(resultado >= 81 && resultado <= 90){
+    } else if (resultado >= 81 && resultado <= 90) {
         return "Diamante"
-    } else if (resultado >= 91 && resultado <=100){
+    } else if (resultado >= 91 && resultado <= 100) {
         return "Lendario"
-    } else if( resultado  >= 100){
+    } else if (resultado >= 100) {
         return "Imortal"
-    } 
+    }
 }
-console.log("O Herói tem um saldo de " + resultado +" pontos e ele está no nivel " + rank )
+console.log("O Herói tem um saldo de " + resultado + " pontos e ele está no nivel " + rank)
